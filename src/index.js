db4dd73c195610ae4121376cf7760d486b54b4f5
Виталий Гутников <vitaly_vg.st_prod@mail.ru>
2022-08-07 17:11:42 +0300

    passing test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+
+  if (matrix === undefined || matrix === 0) {
+    return []; 
+  }
+
+  matrix.forEach((value, index) => {
+    if (index % 2 === 0) {
+      result.push(...value);
+    } else {
+      result.push(...value.reverse());
+    }
+  })
+
+  return result;
 }
+
